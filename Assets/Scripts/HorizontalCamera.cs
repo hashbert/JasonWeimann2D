cbd3d1c1132dfcc580c68f976fd61991fad49a9e
hashbert <obert.chen@gmail.com>
2022-09-08 17:43:33 -0400

    added horizontal follow camera

---
+++ Assets/Scripts/HorizontalCamera.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HorizontalCamera : MonoBehaviour
+{
+    [SerializeField] private Transform _target;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(_target.position.x, transform.position.y, transform.position.z);   
+    }
+}
