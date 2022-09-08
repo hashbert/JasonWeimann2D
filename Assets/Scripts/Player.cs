57718341349634c2157174131400cdeecf16fe4d
hashbert <obert.chen@gmail.com>
2022-09-08 15:12:12 -0400

    added downforce to jump and fixed double jump

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,13 +2,15 @@ using System;
 using UnityEngine;
 public class Player : MonoBehaviour
 {
-    [SerializeField] private float _speed = 8f;
-    [SerializeField] private float _jumpForce =  200f;
+    [SerializeField] private float _speed = 1f;
+    [SerializeField] private float _jumpVelocity = 10f;
     [SerializeField] private int _maxJumps = 2;
     [SerializeField] private Transform _feet;
+    [SerializeField] private float _downPull = 5f;
 
     private Vector3 _startPosition;
     private int _jumpsRemaining;
+    private float _fallTimer;
 
     void Start()
     {
@@ -17,8 +19,12 @@ public class Player : MonoBehaviour
     }
     void Update()
     {
+        var hit = Physics2D.OverlapCircle(_feet.position, 0.1f, LayerMask.GetMask("Default"));
+        bool isGrounded = hit != null;
+
         var horizontal = Input.GetAxis("Horizontal") * _speed;
         var rigidbody2D = GetComponent<Rigidbody2D>();
+
         if (Mathf.Abs(horizontal) >= 1)
         {
             rigidbody2D.velocity = new Vector2(horizontal, rigidbody2D.velocity.y);
@@ -27,25 +33,30 @@ public class Player : MonoBehaviour
         bool walking = horizontal != 0;
         animator.SetBool("Walk", walking);
 
-        var spriteRenderer = GetComponent<SpriteRenderer>();
+        
         if (horizontal != 0)
         {
+            var spriteRenderer = GetComponent<SpriteRenderer>();
             spriteRenderer.flipX = horizontal < 0;
         }
         
         if (Input.GetButtonDown("Fire1") && _jumpsRemaining > 0)
         {
-            rigidbody2D.AddForce(Vector2.up * _jumpForce);
-            _jumpsRemaining -= 1;
+            rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, _jumpVelocity);
+            _jumpsRemaining--;
+            _fallTimer = 0;
         }
-    }
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        var hit = Physics2D.OverlapCircle(_feet.position, 0.1f, LayerMask.GetMask("Default"));
-        if (hit != null)
+        if (isGrounded)
         {
+            _fallTimer = 0;
             _jumpsRemaining = _maxJumps;
+        }
+        else
+        {
+            _fallTimer += Time.deltaTime;
+            var downForce = _downPull * _fallTimer * _fallTimer;
+            rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, rigidbody2D.velocity.y - downForce);
         }
     }
     internal void ResetToStart()
