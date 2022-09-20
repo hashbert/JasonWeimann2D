9a1aea66e37d9a5f515a876726867ee03ff03674
hashbert <obert.chen@gmail.com>
2022-09-20 10:57:20 -0400

    added unity events to push button switch

--- Assets/Scripts/PushButtonSwitch.cs
+++ Assets/Scripts/PushButtonSwitch.cs
@@ -5,21 +5,43 @@ using UnityEngine.Events;
 
 public class PushButtonSwitch : MonoBehaviour
 {
-    [SerializeField] private Sprite _downSprite;
-    [SerializeField] private SpriteRenderer _spriteRenderer;
-    [SerializeField] private UnityEvent _onEnter;
+    [SerializeField] private Sprite _pressedSprite;
+    [SerializeField] private UnityEvent _onPressed; 
+    [SerializeField] private UnityEvent _onReleased;
+    private SpriteRenderer _spriteRenderer;
+    private Sprite _releasedSprite;
 
     private void Awake()
     {
         _spriteRenderer = GetComponent<SpriteRenderer>();
+        _releasedSprite = _spriteRenderer.sprite;
+        BecomeReleased();
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         var player = collision.GetComponent<Player>();
         if (player == null) return;
 
-        _spriteRenderer.sprite = _downSprite;
+        BecomePressed();
+    }
 
-        _onEnter?.Invoke();
+    private void BecomePressed()
+    {
+        _spriteRenderer.sprite = _pressedSprite;
+        _onPressed?.Invoke();
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        var player = collision.GetComponent<Player>();
+        if (player == null) return;
+
+        BecomeReleased();
+    }
+
+    private void BecomeReleased()
+    {
+        _spriteRenderer.sprite = _releasedSprite;
+        _onReleased?.Invoke();
     }
 }
