fe63ab0bbfa9adad4aa7ffb03540fb33acce4a6c
hashbert <obert.chen@gmail.com>
2022-10-02 17:32:15 -0400

    Update FallingPlatform.cs
    implemented wiggling platform

--- Assets/Scripts/FallingPlatform.cs
+++ Assets/Scripts/FallingPlatform.cs
@@ -7,8 +7,13 @@ public class FallingPlatform : MonoBehaviour
 {
     public bool PlayerInside;
 
-    private HashSet<Player> _players = new HashSet<Player>();
+    private HashSet<Player> _playersInTrigger = new HashSet<Player>();
+    private Vector3 _initialPosition;
 
+    private void Start()
+    {
+        _initialPosition = transform.position;
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         var player = collision.GetComponent<Player>();
@@ -16,16 +21,27 @@ public class FallingPlatform : MonoBehaviour
 
         PlayerInside = true;
 
-        _players.Add(player);
-
-        StartCoroutine(WiggleAndFall());
+        _playersInTrigger.Add(player);
+        if (_playersInTrigger.Count == 1)
+        {
+            StartCoroutine(WiggleAndFall());
+        }
     }
 
     private IEnumerator WiggleAndFall()
     {
         print("waiting to wiggle");
         yield return new WaitForSeconds(.25f);
-        print("wiggling");
+        float wiggleTimer = 0f;
+        while (wiggleTimer < 1f)
+        {
+            float randomX = UnityEngine.Random.Range(-.05f, 0.05f);
+            float randomY = UnityEngine.Random.Range(-.05f, 0.05f);
+            transform.position = _initialPosition + new Vector3(randomX, randomY);
+            float randomDelay = UnityEngine.Random.Range(.005f, .01f);
+            yield return new WaitForSeconds(randomDelay);
+            wiggleTimer += randomDelay;
+        }
         yield return new WaitForSeconds(1f);
         print("falling");
         yield return new WaitForSeconds(3f);
@@ -37,11 +53,12 @@ public class FallingPlatform : MonoBehaviour
         var player = collision.GetComponent<Player>();
         if (player == null) return;
 
-        _players.Remove(player);
+        _playersInTrigger.Remove(player);
 
-        if (_players.Count <= 0)
+        if (_playersInTrigger.Count <= 0)
         {
             PlayerInside = false;
+            StopCoroutine(WiggleAndFall());
         }
     }
 }
