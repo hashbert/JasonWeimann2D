d5a9f976b214cc6ed02f56ddfdf6bbb2f68bbeec
hashbert <obert.chen@gmail.com>
2022-10-05 18:58:16 -0400

    Update Collectible.cs

--- Assets/Scripts/Collectible.cs
+++ Assets/Scripts/Collectible.cs
@@ -5,7 +5,7 @@ public class Collectible: MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         var player = collision.GetComponent<Player>();
-        if (player != null)
+        if (player == null)
         {
             return;
         }
