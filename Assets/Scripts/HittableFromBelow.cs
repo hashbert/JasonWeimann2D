3842ffeb9cecaf34d0ba28c7ceaae88b64c3e255
hashbert <obert.chen@gmail.com>
2022-10-10 22:21:13 -0400

    used inheritance to make boxes work
    17E

--- Assets/Scripts/HittableFromBelow.cs
+++ Assets/Scripts/HittableFromBelow.cs
@@ -1,26 +1,42 @@
 ﻿using System;
 using UnityEngine;
 
-public class HittableFromBelow: MonoBehaviour
+public class HittableFromBelow : MonoBehaviour
 {
     [SerializeField] protected Sprite _usedSprite;
+    [SerializeField] private Animator _animator;
 
     protected virtual bool CanUse => true;
     //protected virtual bool CanUse { get;  } = true; (same as above)
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (CanUse==false) return;
+        if (CanUse == false) return;
         var player = collision.collider.GetComponent<Player>();
         if (!player) { return; }
 
         if (collision.contacts[0].normal.y > 0)
         {
+            PlayAnimation();
             Use();
-
             if (CanUse == false)
             {
                 GetComponent<SpriteRenderer>().sprite = _usedSprite;
             }
+
+        }
+
+    }
+
+    private void PlayAnimation()
+    {
+        if (_animator != null)
+        {
+            _animator.SetTrigger("Use");
+            print("hello");
         }
     }
 
